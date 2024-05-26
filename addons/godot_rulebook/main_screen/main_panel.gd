d274c2764f016c17c8795a209f73d7c4c3a62396
Guilherme <guilhermeferreira2529@hotmail.com>
2024-05-26 14:29:56 -0300

    :sparkles: Save and load rulebooks from disk feature (still buggy) + implements "+ Rulebook" button

---
+++ addons/godot_rulebook/main_screen/main_panel.gd
@@ -0,0 +1,29 @@
+@tool
+extends PanelContainer
+
+const RULEBOOK_TAB := preload("res://addons/godot_rulebook/main_screen/components/rulebook_tab.tscn")
+var suffix: int = 1
+
+func get_rulebooks() -> Array[Control]:
+	var result: Array[Control]
+	for rulebook: Control in %TabContainer.get_children():
+		if rulebook.name != "+ Rulebook":
+			result.append(rulebook)
+	return result
+
+
+func load_rulebook(rulebook: Control, custom_position: int = 0) -> void:
+	%TabContainer.add_child(rulebook)
+	%TabContainer.move_child(rulebook, custom_position)
+
+
+func create_rulebook() -> void:
+	var new_rulebook = RULEBOOK_TAB.instantiate()
+	new_rulebook.name = "My Rulebook " + str(suffix)
+	suffix += 1
+	load_rulebook(new_rulebook, %"+ Rulebook".get_index())
+
+
+func _on_tab_container_tab_clicked(tab: int):
+	if tab == %TabContainer.get_tab_idx_from_control(%"+ Rulebook"):
+		create_rulebook()
--- addons/godot_rulebook/plugin.gd
+++ addons/godot_rulebook/plugin.gd
@@ -4,6 +4,7 @@ extends EditorPlugin
 const MAIN_PANEL := preload("res://addons/godot_rulebook/main_screen/main_panel.tscn")
 const ICON := preload("res://addons/godot_rulebook/main_screen/icon.svg")
 const AUTOLOAD_NAME := "RulebooksManager"
+const SAVED_RULEBOOKS_PATH := "res://addons/godot_rulebook/saved_rulebooks/"
 
 var main_panel_instance: Node
 
@@ -13,8 +14,24 @@ func _enter_tree() -> void:
 	main_panel_instance = MAIN_PANEL.instantiate()
 	# Add the main panel to the editor's main viewport.
 	EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
+	load_rulebooks_from_disk()
 	# Hide the main panel. Very much required.
 	_make_visible(false)
+
+
+func load_rulebooks_from_disk() -> void:
+	var dir = DirAccess.open(SAVED_RULEBOOKS_PATH)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			var scene: PackedScene = ResourceLoader.load(SAVED_RULEBOOKS_PATH + file_name)
+			var result = scene.instantiate()
+			if result:
+				main_panel_instance.load_rulebook(result)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
 
 # Clean-up of the plugin.
 func _exit_tree() -> void:
@@ -30,6 +47,21 @@ func _has_main_screen() -> bool:
 func _make_visible(visible: bool) -> void:
 	if main_panel_instance:
 		main_panel_instance.visible = visible
+
+
+func _save_external_data() -> void:
+	for rulebook: Control in main_panel_instance.get_rulebooks():
+		print(rulebook.get_tree_string_pretty())
+		for child: Control in rulebook.get_children():
+			child.propagate_call("set_owner", [rulebook])
+			child.propagate_call("et_scene_file_path", [""])
+		
+		var scene: PackedScene = PackedScene.new()
+		var result = scene.pack(rulebook)
+		if result == OK:
+			var error = ResourceSaver.save(scene, SAVED_RULEBOOKS_PATH + rulebook.name + ".tscn")
+			if error != OK:
+				push_error("An error occurred while saving the Rulebook to disk.")
 
 
 func _get_plugin_name() -> String:
